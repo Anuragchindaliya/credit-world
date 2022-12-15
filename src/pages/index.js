2f4cfe17a226bd143c8492f55a53b0d4068096e1
Anuragchindaliya <ak1148383@gmail.com>
2022-12-15 21:00:53 +0530

    add: footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,21 @@ import AddressCta from "../components/CTA/AddressCta";
 import HomeCta from "../components/CTA/HomeCta";
 import HomeCta2 from "../components/CTA/HomeCta2";
 import HomeFeatures from "../components/Features/Home";
+import Footer from "../components/Footer";
 import Services from "../components/Services";
 import HomeSlider from "../components/slider/HomeSlider";
 
 const Home = () => {
   return (
-    <section >
+    < >
       {/* <HomeSlider /> */}
       <HomeFeatures />
       <HomeCta />
       <HomeCta2 />
       <Services />
       <AddressCta />
-    </section>
+      <Footer />
+    </>
   );
 };
 
