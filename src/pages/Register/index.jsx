69f6b3b9105ddd4d1c7cf6b3b31eeac0d2f63e6b
Anuragchindaliya <ak1148383@gmail.com>
2023-01-01 18:52:29 +0530

    add: reverse column in mobile

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -10,16 +10,16 @@ const Register = () => {
   return (
     <>
       <div className="bg-gray ">
-        <div className="container py-[20px] md:py-[40px]">
-          <div className="row">
+        <div className="container py-1">
+          <div className="my-2">
+            <Link to={"/"}>
+              <i className="fa fa-home text-blue-900" />
+            </Link>
+            <i className="fa fa-chevron-right px-1" />
+            <span>HDFC Bank Credit Card</span>
+          </div>
+          <div className="row flex-col-reverse sm:flex-row ">
             <div className="col-lg-7">
-              <div className="my-1">
-                <Link to={"/"}>
-                  <i className="fa fa-home text-blue-900" />
-                </Link>
-                <i className="fa fa-chevron-right px-1" />
-                <span>HDFC Bank Credit Card</span>
-              </div>
               <h1 className="text-2xl md:text-3xl font-bold text-blue-900 mb-2">
                 HDFC Bank Credit Card
               </h1>
@@ -30,7 +30,7 @@ const Register = () => {
               </p>
               <CardSlider />
             </div>
-            <div className="col-lg-5">
+            <div className="col-lg-5 mb-5 ">
               <ApplyForm />
             </div>
           </div>
